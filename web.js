c3a188433200b27c2ec16591b0fa2085490c9395
vinoopchandra <vinoopchandra@gmail.com>
2013-07-10 21:53:45 +0000

    Modified web.js to read content from file on disc2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-  
+  var fs = require('fs');
   var bufr = fs.readFileSync(index.html);
   response.send(bufr.toString('utf-8'));
 });
