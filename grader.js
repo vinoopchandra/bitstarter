b904719cd9c7f88410d47fb4f0ad3d64cac01a4b
vinoopchandra <vinoopchandra@gmail.com>
2013-07-16 16:32:15 +0000

    Added url input

--- grader.js
+++ grader.js
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
@@ -24,6 +25,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest=require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,9 +67,11 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_line>', 'url string')  
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
+    var inputurl=checkHtmlFile(rest.get(program.url),program.checks);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
